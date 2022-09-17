6961c3b6db2c9629aa6f24cefd4148a60986fc1d
Luuk Bobeldijk <luuk.bobeldijk@dds.nl>
2022-09-17 22:03:34 +0200

    Update drivers

--- drivers/silabs/si3226x/base.c
+++ drivers/silabs/si3226x/base.c
@@ -1,3 +1,16 @@
+/*
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2 as published by the
+ * Free Software Foundation. See the LICENSE file included with
+ * this program for more details.
+ */
+
 #include <linux/interrupt.h>
 
 // We consider diasy chain case only. 
@@ -181,10 +194,15 @@ static int __init si3226x_init( void )
 	return 0;
 }
 
+static void __exit si3226x_exit(void)
+{
+
+}
+
 MODULE_DESCRIPTION("Silicon Labs Si3226x FXS Driver");
 
 MODULE_ALIAS("Si3226x");
 MODULE_LICENSE("GPL v2");
 
 module_init(si3226x_init);
-module_exit(si3226x_cleanup);
+module_exit(si3226x_exit);
